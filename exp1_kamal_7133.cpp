d82f45113f65c8c4d4aa4354f4b88d3b9b347609
Kamalkaur2006 <160565709+Kamalkaur2006@users.noreply.github.com>
2024-03-22 10:25:34 +0530

    Add files via upload

---
+++ exp1_kamal_7133.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+		int main(){
+			int number , count = 0;
+;
+			
+			for (int i = 0; i < 5;i++){
+				cout<< " number" <<endl;
+			cin>>number;
+			
+			if(number %8 == 0){
+				count++;
+			}
+	}
+			
+		cout << "Total "<< count << "  valid numbers." << endl;
+	
+
+			return 0;
+			}
+			
+
